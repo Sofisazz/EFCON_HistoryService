f6eb3373ddcd056f3989083e8858a229c129ea7e
Sof <gurskayasofia7@gmail.com>
2026-03-09 22:01:08 +0300

    fix: removed annotation @Service

--- src/main/java/com/example/historyservice/service/NoteService.java
+++ src/main/java/com/example/historyservice/service/NoteService.java
@@ -3,9 +3,7 @@ package com.example.historyservice.service;
 import com.example.historyservice.dto.NoteDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Service;
 
-@Service
 public interface NoteService {
     Page<NoteDto> findAllRecipes(Pageable pageable);
     NoteDto findNoteById(int id);
